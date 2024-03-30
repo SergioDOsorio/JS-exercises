e48234156b5429b6a8bea0d6e8cfb16e4b2d3afc
Sergio Osorio <sdosoriogonzalez19@outlook.es>
2024-03-30 12:59:35 -0500

    First commit

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import './App.css'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Home from './pages/Home';
+
+function App() {
+  return (
+    <div>
+      <h1>Hola</h1>
+    </div>
+  )
+}
+
+export default App
